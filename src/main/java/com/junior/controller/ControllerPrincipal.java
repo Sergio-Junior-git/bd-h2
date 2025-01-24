58e44d999d7eb578c834ea8825b1ca1abc7dc80a
Sergio-Junior-git <sergio.galdino.junior01@gmail.com>
2025-01-24 11:07:36 -0300

    link com o formulario do front end

--- src/main/java/com/junior/controller/ControllerPrincipal.java
+++ src/main/java/com/junior/controller/ControllerPrincipal.java
@@ -2,8 +2,13 @@ package com.junior.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.junior.model.Clientes;
@@ -21,6 +26,14 @@ public class ControllerPrincipal {
     @GetMapping
     public List<Clientes> list() {
         return clientesRepository.findAll();
+    }
+
+    //@RequestMapping(method = RequestMapping.Post)
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Clientes create(@RequestBody Clientes clientes) {
+        return clientesRepository.save(clientes);
+      //return ResponseEntity.status(HttpStatus.CREATED).body(clientesRepository.save(clientes));
     }
     
 }
